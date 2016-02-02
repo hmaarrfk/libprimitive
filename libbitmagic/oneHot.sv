48cff444264b88c0c0a33b56cd56ef70a05fa8eb
Malte Vesper <malte.vesper@gmx.net>
2016-02-02 09:44:42 +0000

    Added reference to alternative Altera cookbook method

--- libbitmagic/oneHot.sv
+++ libbitmagic/oneHot.sv
@@ -13,6 +13,22 @@ module oneHotDecoder #(
 				binary |= i; //ORing here, to avoid the generation of a priority decoder, since the system does not know it is a one hot vector
 			end
 		end
+		
+		//TODO: is Altera cookbooks method better? (see below)
+		/*
+		 * //Code taken from the altera cookbook:
+		 * //baeckler - 11-14-2006
+		 * 	for (j=0; j<BIN_WIDTH; j=j+1)
+		 * begin : jl
+		 * 		wire [ONEHOT_WIDTH-1:0] tmp_mask;
+		 * 		for (i=0; i<ONEHOT_WIDTH; i=i+1)
+		 * 		begin : il
+		 * 			assign tmp_mask[i] = i[j];
+		 * 		end	
+		 * 		assign bin[j] = |(tmp_mask & onehot);
+		 * end
+		 */
 	end
 	
 endmodule
+
